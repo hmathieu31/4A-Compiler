ca0620f6bfede268ffc364ce969eeaf5339e581a
Aubut Guillaume <aubut@insa-11289>
2022-03-10 12:12:56 +0100

    implementation addSymbol deleteSymbol isEmpty pour la table des symboles

--- src/symbolTable.c
+++ src/symbolTable.c
@@ -10,26 +10,57 @@
  */
 
 #include "symbolTable.h"
-
-typedef struct symbolTable
-{
-    symbol sym;
-    symbol* prev;
-} symbolTable;
-
+#include "<stdio.h>"
+#include "<sting.h>"
 
 typedef struct symbol
 {
     char* symbolName;
-    int index;
     type typ;
     int depth;
 }symbol;
 
-int addSymbol(char* symbolName, enum type typ, int depth);
-
-int deleteSymbol(char* symbolName);
+int isEmpty()
+{
+    if (symbolTable.topIndex==-1)
+    {
+        result=1;
+    }
+    else
+    {
+        result=0;
+    };
+    return result;
+};
     
+int addSymbol(char* symbolName, enum type typ, int depth)
+{
+    if isEmpty()
+    {
+        symbolTable.symbolArray[0]={symbolName,typ,0};
+        symbolTable.topIndex=0;
+    }
+    else
+    {
+        symbolTable.symbolArray[symbolTable.topIndex+1]={symbolName,index,typ,depth};
+        (symboleTable.topIndex)++;      
+    }
+    return 0
+};
+    
+int deleteSymbol()
+{
+    if isEmpty()
+    {
+        printf("Nothing to delete, empty stack!\n");
+    }
+    else
+    {
+    symboleTable.topIndex--;
+    }
+    return 0
+};
+        
 
 int isSymbolPresent(char* symbol);
     

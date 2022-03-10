ae728b5691a640e136ab6e20c18f096c2eb62bb6
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-03-10 23:25:20 +0100

    🚧 symbolTable - initTable is untested - isSymbolPresent and deleteFromChangeScope must be redone

--- src/symbolTable.c
+++ src/symbolTable.c
@@ -4,25 +4,53 @@
  * @brief Symbol table implementation for the compiler
  * @version 0.1
  * @date 2022-03-10
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #include "symbolTable.h"
 
+#include <stdio.h>
+#include <string.h>
+
+symbolTable table;
+
+int initTable() {
+    table.topIndex = -1;
+    symbol symInit = {
+        NULL,
+        NULL,
+        -1
+    };
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        table.symbolArray[i] = symInit;
+    }
+    return 0;
+}
+
 int addSymbol(char* symbolName, enum type typ, int depth);
 
 int deleteSymbol(char* symbolName);
-    
 
 int isSymbolPresent(char* symbol) {
-    
+    int isPresent = 0;
+    if (!isEmpty()) {
+        while (table.prev != NULL && !isPresent) {
+            isPresent = strcmp(table.sym.symbolName, symbol);
+        }
+    }
+    return isPresent;
 }
-    
 
-int deleteFromChangeScope();
-    
+int deleteFromChangeScope() {
+    if (!isEmpty()) {
+        symbol sym = table.sym;
+        int scope = sym.depth;
+        while (table.prev != NULL && sym.depth == scope) {
+        }
+    }
+}
 
 int getAddressSymbol(char* symbol);
-    

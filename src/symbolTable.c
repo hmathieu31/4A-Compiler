263d031ae8e3ef16b37a48b0a74b9ccc51d965bc
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-03-11 01:45:50 +0100

    🚧 Fix some various code isues on the functions

--- src/symbolTable.c
+++ src/symbolTable.c
@@ -13,16 +13,17 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 symbolTable table;
 
-int initTable() {
+int initTable()
+{
     table.topIndex = -1;
     symbol symInit = {
         NULL,
-        NULL,
-        -1
-    };
+        t_int,
+        -1};
     for (int i = 0; i < TABLE_SIZE; i++)
     {
         table.symbolArray[i] = symInit;
@@ -30,56 +31,53 @@ int initTable() {
     return 0;
 }
 
-int deleteSymbol();
-
 int isEmpty()
 {
-    if (symbolTable.topIndex==-1)
+    int empty = 0;
+    if (table.topIndex == -1)
     {
-        result=1;
+        empty = 1;
     }
-    else
-    {
-        result=0;
-    };
-    return result;
+    return empty;
 };
-    
-int addSymbol(char* symbolName, enum type typ, int depth)
+
+int addSymbol(char *symbolName, enum type typ, int depth)
+{
+    if (table.topIndex == 1024) // The symbol table being limited to 1024 symbols, an error must be handled if trying to add another symbol
+    {
+        fprintf(stderr, "Symbol table is full. Program cannot compile!\n");
+        exit(-1);
+    }
+    symbol sym = {symbolName, typ, 0};
+    table.symbolArray[table.topIndex + 1] = sym;
+    table.topIndex += 1;
+    return 0;
+};
+
+int deleteSymbol()
 {
     if (isEmpty())
     {
-        symbolTable.symbolArray[0]={symbolName,typ,0};
-        symbolTable.topIndex=0;
+        fprintf(stderr, "Nothing to delete, empty stack!\n");
+        exit(-1);
     }
     else
     {
-        symbolTable.symbolArray[symbolTable.topIndex+1]={symbolName,index,typ,depth};
-        (symboleTable.topIndex)++;      
-    }
-    return 0
-};
-   
-int deleteSymbol()
-{
-    if isEmpty()
-    {
-        printf("Nothing to delete, empty stack!\n");
-    }
-    else
-    {
-    symboleTable.topIndex--;
+        table.topIndex--;
     }
     return 0;
 };
 
-int deleteFromChangeScope() {
-    if (!isEmpty()) {
-        symbol sym = table.sym;
-        int scope = sym.depth;
-        while (table.prev != NULL && sym.depth == scope) {
-        }
-    }
-}
+// int deleteFromChangeScope()
+// {
+//     if (!isEmpty())
+//     {
+//         symbol sym = table.sym;
+//         int scope = sym.depth;
+//         while (table.prev != NULL && sym.depth == scope)
+//         {
+//         }
+//     }
+// }
 
-int getAddressSymbol(char* symbol);
+int getAddressSymbol(char *symbol);

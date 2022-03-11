5c7651e6bc7ec7c61191d412e7b792d4071332e3
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-03-11 12:02:36 +0100

    🔨 Better test aestetics

--- test.ps1
+++ test.ps1
@@ -2,17 +2,17 @@ foreach ($item in Get-ChildItem .\tests\tests_ok\) {
     Get-Content $item | .\bin\comp.exe 2>&1 | Out-Null
     $test = "Test ok - " + $item.Name
     if ($?) {
-        Write-Output -InputObject $test" - passed successfully"
+        Write-Output -InputObject $test" - passed successfully ✅"
     } else {
-        Write-Output -InputObject $test" - failed"
+        Write-Output -InputObject $test" - failed ❌"
     }
 }
 foreach ($item in Get-ChildItem .\tests\tests_ko) {
     Get-Content $item | .\bin\comp.exe 2>&1 | Out-Null
     $test = "Test ko - " + $item.Name
     if ($?) {
-        Write-Output -InputObject $test" - passed successfully"
+        Write-Output -InputObject $test" - passed successfully ✅"
     } else {
-        Write-Output -InputObject $test" - failed"
+        Write-Output -InputObject $test" - failed ❌"
     }
 }

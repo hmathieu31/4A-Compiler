8083d34e72488562bcc9f731fe689f2be2c3b26c
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-03-12 21:33:29 +0100

    🐛 Fix dumb bug in test script

--- test.ps1
+++ test.ps1
@@ -1,7 +1,8 @@
 foreach ($item in Get-ChildItem .\tests\tests_ok\) {
     Get-Content $item | .\bin\comp.exe 2>&1 | Out-Null
+    $execSuccessful = $?
     $test = "Test ok - " + $item.Name
-    if ($?) {
+    if ($execSuccessful -eq $true) {
         Write-Output -InputObject $test" - passed successfully ✅"
     } else {
         Write-Output -InputObject $test" - failed ❌"
@@ -9,8 +10,9 @@ foreach ($item in Get-ChildItem .\tests\tests_ok\) {
 }
 foreach ($item in Get-ChildItem .\tests\tests_ko) {
     Get-Content $item | .\bin\comp.exe 2>&1 | Out-Null
+    $execSuccessful = $?
     $test = "Test ko - " + $item.Name
-    if ($?) {
+    if ($execSuccessful -eq $false) {
         Write-Output -InputObject $test" - passed successfully ✅"
     } else {
         Write-Output -InputObject $test" - failed ❌"

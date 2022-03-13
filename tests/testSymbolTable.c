a7600fd546318cc9cf1c8869d7b305c2dd290cfd
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-03-13 23:36:28 +0100

    ✅ getAddressSymbol

--- tests/testSymbolTable.c
+++ tests/testSymbolTable.c
@@ -7,7 +7,7 @@ int test_addOneSymbol()
     addSymbol("a", t_int, 0);
     if (isEmpty())
     {
-        exit(1);
+        return(1);
     }
 
     return 0;
@@ -18,12 +18,12 @@ int test_addMultipleSymbols()
     char buf[256];
     for (int i = 0; i < 1023; i++)
     {
-        itoa(i, buf, sizeof(buf));
+        sprintf(buf, "%d", i);
         addSymbol(buf, t_int, 0);
     }
     if (isEmpty())
     {
-        exit(1);
+        return(1);
     }
     return 0;
 }
@@ -32,12 +32,12 @@ int test_isEmpty()
 {
     if (!isEmpty())
     {
-        exit(1);
+        return(1);
     }
     addSymbol("a", t_int, 0);
     if (isEmpty())
     {
-        exit(1);
+        return(1);
     }
     return 0;
 }
@@ -47,7 +47,7 @@ int test_TableOverflow()
     test_addMultipleSymbols();
     if (addSymbol("a", t_int, 0) != -1)
     {
-        exit(1);
+        return(1);
     }
     return 0;
 }
@@ -58,13 +58,40 @@ int test_deleteSymbol()
     addSymbol("a", t_int, 0);
     if (isEmpty())
     {
-        exit(1);
+        return(1);
     }
     deleteSymbol("a");
     if (!isEmpty())
     {
-        exit(1);
+        return(1);
     }
+    return 0;
+}
+
+int test_getAddressSymbol()
+{
+    initTable();
+    addSymbol("toto", t_int, 0);
+    addSymbol("a", t_int, 0);
+    addSymbol("b", t_int, 0);
+    addSymbol("c", t_int, 0);
+    addSymbol("pouf", t_int, 1);
+    int add = getAddressSymbol("toto");
+    if (add != 0)
+    {
+        return(1);
+    }
+    add = getAddressSymbol("pouf");
+    if(add != 4)
+    {
+        return(1);
+    }
+    add = getAddressSymbol("b");
+    if (add != 2)
+    {
+        return(1);
+    }
+    
     return 0;
 }
 
@@ -122,8 +149,15 @@ int main(int argc, char const *argv[])
         printf("Test - test_deleteSymbol - PASSED ✅ \n");
     }
     
-    
-    
+    if (test_getAddressSymbol())
+    {
+        fprintf(stderr, "Test - test_getAddressSymbol- FAILED ❌ \n");
+        allTestsPassed = 0;
+    } else
+    {
+        printf("Test - test_getAddressSymbol - PASSED ✅ \n");
+    }
+
 
     if (allTestsPassed)
     {

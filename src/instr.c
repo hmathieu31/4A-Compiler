8a947b7ff11e1c318a4d6a61f4eb3284a0f2b52d
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-03-19 23:56:34 +0100

    ✨ 🎨 Switch instructions from string to struct type
    + Implement stringOfInstruction method
    - Untested

--- src/instr.c
+++ src/instr.c
@@ -1,12 +1,12 @@
 /**
  * @file instr.c
  * @author Hugo Mathieu (hmathieu@insa-toulouse.fr) Guillaume Aubut (@Ger0th)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-03-19
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 #include "instr.h"
 
@@ -16,32 +16,68 @@
 
 #define TAILLE 1025
 
-char* instrArray[TAILLE];
+instruction instrArray[TAILLE];
 
-void initArray() {
-    for (int i = 0; i < TAILLE - 1; i++) {
-        instrArray[i] = "";
+void initArray()
+{
+    instruction instrInit;
+    for (int i = 0; i < 2; i++)
+    {
+        instrInit.ops[i] = -1;
+    }
+    for (int i = 0; i < TAILLE - 1; i++)
+    {
+        instrArray[i] = instrInit;
     }
 }
 
-int addInstruction(char* instr, int sizeofInstr) {
+int addInstruction(instruction instr)
+{
     int i = 0;
-    while (i < TAILLE - 1 && strcmp(instrArray[i], "")) {
+    while (i < TAILLE - 1 && instrArray[i].ops[0] == -1)
+    {
         i++;
     }
-    if (i == TAILLE - 1) {
-        fprintf(stderr, "Maximum array size exceeded\n");
+    if (i == TAILLE - 1)
+    {
+        fprintf(stderr, "Maximum instruction array size exceeded\n");
         exit(1);
     }
-    char* nameInstr = (char*)malloc(sizeofInstr);
-    strncpy(nameInstr, instr, sizeofInstr);
-    instrArray[i] = nameInstr;
+    instrArray[i] = instr;
     return 0;
 }
 
-int printTable() {
-    for (int i = 0; i < TAILLE - 1; i++) {
-        printf(instrArray[i]);
+int printTable()
+{
+    for (int i = 0; i < TAILLE - 1; i++)
+    {
+        printf(stringOfInstruction(instrArray[i]));
     }
     return 0;
+}
+
+char *stringOfInstruction(instruction instruction)  // TODO #3 Test the function
+{
+    if (instruction.ops[0] == -1)
+    {
+        fprintf(stderr, "No instruction passed");
+    }
+
+    char *str_out = "Instruction: ";
+    char *str_operator = operator_string[instruction.ope];
+    strcat(str_out, str_operator);
+    strcat(str_out, "  ");
+
+    char *operands = "";
+    int i = 0;
+    while (instruction.ops[i] != -1 && i < 3)
+    {
+        int length = sprintf(NULL, 0, "%d", instruction.ops[i]);
+        char *op = malloc(length + 3);
+        snprintf(op, length + 1, "%d  ", instruction.ops[i]);
+        strcat(operands, op);
+        i++;
+    }
+
+    return str_out;
 }
--- src/instr.h
+++ src/instr.h
@@ -1,12 +1,12 @@
 /**
  * @file instr.h
  * @author Hugo Mathieu (hmathieu@insa-toulouse.fr) Guillaume Aubut (@Ger0th)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-03-19
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,26 +15,57 @@
 #define INSTR_H
 
 #define TAILLE 1025
+#define OPERATORS \
+    C(MOV)        \
+    C(MOV_I)      \
+    C(ADD)        \
+    C(SUB)        \
+    C(MUL)        \
+    C(DIV)
+#define C(x) x,
+typedef enum operator
+{
+    OPERATORS TOP
+} operator;
+#undef C
+
+#define C(x) #x,
+
+const char *const operator_string[] = {OPERATORS};
+
+typedef struct instruction
+{
+    operator ope;
+    int ops[2];
+} instruction;
 
 /**
- * @brief Initializes the array of instructions to ""
- * 
+ * @brief Initializes the array of instructions to instructions without specified operator and operands to -1
+ *
  */
 void initArray();
 
 /**
  * @brief Adds an instruction (string) to the array
- * 
+ *
  * @param instr Asm instruction to add (string)
  * @return 0 if executed successfully.
  */
-int addInstruction(char* instr, int sizeofInstr);
+int addInstruction(instruction instr);
 
 /**
  * @brief Prints the entire array of instructions.
- * 
+ *
  * @return 0 if executed successfully.
  */
 int printTable();
+
+/**
+ * @brief Function to convert an instruction to a string.
+ *
+ * @param instr Structure instruction to convert.
+ * @return Formatted string representation of the instruction.
+ */
+char *stringOfInstruction(instruction instr);
 
 #endif

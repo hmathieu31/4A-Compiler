78684d06385979178dfcd4f60d4b9151171986cd
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-03-19 10:36:04 +0100

    🧪 Test with Scopes (if and while)

---
+++ tests/tests_ok/test_WithScopes.c
@@ -0,0 +1,14 @@
+
+main() {
+    int a = 3;
+    int b = 4;
+    if (a == 3 && b == 4)
+    {
+        int o = 5;
+        while (a != 3) {
+            int c = a;
+            int d = b;
+        }
+    }
+    
+}

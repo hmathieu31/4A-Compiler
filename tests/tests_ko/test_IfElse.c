c3f3cbebc8cfac3ff787e1232e4cbee96ba31a53
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-04-14 16:08:17 +0200

    ✅ Pass tests on If and While

---
+++ tests/tests_ko/test_IfElse.c
@@ -0,0 +1,12 @@
+main() {
+    int a = 3;
+    int b = 4;
+    if (a == 3 && b == 4)
+    {
+        int o = 5;
+    } else
+    {
+        int z = 6;
+    }
+    printf(b);
+}
---
+++ tests/tests_ok/test_While.c
@@ -0,0 +1,12 @@
+main() {
+    int i = 0;
+    while (i < 10)
+    {
+        i = i + 1;
+    }
+    int a;
+    int b;
+    a = b + 3;
+    int c = a > 2;
+    printf(a);
+}

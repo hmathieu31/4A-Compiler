e89e27ddb1961cc94c9862e47f8c5dd9eecfc363
Ger0th <aubutguillaume@gmail.com>
2022-04-14 12:19:43 +0200

    ✨ Implement Pipeline

---
+++ FPGA_Program/FPGA_Program.srcs/sources_1/new/Pipeline.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 14.04.2022 11:30:11
+-- Design Name: 
+-- Module Name: Pipeline - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Pipeline is
+    Port ( Ain : in STD_LOGIC_VECTOR (7 downto 0);
+           Bin : in STD_LOGIC_VECTOR (7 downto 0);
+           Cin : in STD_LOGIC_VECTOR (7 downto 0);
+           OPin : in STD_LOGIC_VECTOR (3 downto 0);
+           Aout : out STD_LOGIC_VECTOR (7 downto 0);
+           Bouftou : out STD_LOGIC_VECTOR (7 downto 0);     
+           Cout : out STD_LOGIC_VECTOR (7 downto 0);
+           OPout : out STD_LOGIC_VECTOR (3 downto 0);
+           CLK : in STD_LOGIC);
+end Pipeline;
+
+architecture Behavioral of Pipeline is
+begin
+    process
+    begin
+        wait until CLK'event and CLK='1';
+        Aout<=Ain;
+        Bouftou<=Bin;
+        Cout<=Cin;
+        OPout<=OPin;
+    end process;
+
+end Behavioral;

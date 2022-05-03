7ce7ba303fc1be1cd37d685ccd1af31619d3109b
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-05-03 13:56:09 +0200

    🧪 Func with params
    The return value is not correctly handled

--- tests/tests_ok/test_FuncWithParams.c
+++ tests/tests_ok/test_FuncWithParams.c
@@ -1,12 +1,13 @@
+
 int fun(int x, int y) {
     int a = x;
-    return a ;
+    return a + y + 1;
 }
+
 main() {
     const int a = 3;
     int x;
     int b = fun(2, 1);
-    int d = f(x);
-    int e = g(2 + 3);
-    printf(e);
+    int e = fun(a, b);
+    printf(b);
 }
